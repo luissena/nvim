36f46f1d2070ffd973c5bb47b1bd4fcbbb39f43d
luissena <luis.senarode@gmail.com>
2026-01-31 08:12:55 -0300

    feat(plugins): add treesitter syntax highlighting

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "vim",
+      "lua",
+      "vimdoc",
+      "html",
+      "css",
+      "dart",
+      "javascript",
+      "typescript",
+      "tsx",
+      "json",
+      "c_sharp",
+      "dockerfile",
+      "yaml",
+      "markdown",
+      "markdown_inline",
+    },
+  },
+}
