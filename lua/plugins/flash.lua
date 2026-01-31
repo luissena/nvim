465aafcfd23e13494f7333e5d3d38323bcabf683
luissena <luis.senarode@gmail.com>
2026-01-31 08:25:58 -0300

    fix(plugins): improve flash.nvim highlight contrast

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -7,7 +7,23 @@ return {
         jump_labels = true,
       },
     },
+    highlight = {
+      backdrop = true,
+      groups = {
+        match = "FlashMatch",
+        current = "FlashCurrent",
+        backdrop = "FlashBackdrop",
+        label = "FlashLabel",
+      },
+    },
   },
+  config = function(_, opts)
+    require("flash").setup(opts)
+    vim.api.nvim_set_hl(0, "FlashLabel", { fg = "#000000", bg = "#ff007c", bold = true })
+    vim.api.nvim_set_hl(0, "FlashMatch", { fg = "#89ddff", underline = true })
+    vim.api.nvim_set_hl(0, "FlashCurrent", { fg = "#ffffff", bg = "#ff966c" })
+    vim.api.nvim_set_hl(0, "FlashBackdrop", { fg = "#545c7e" })
+  end,
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
