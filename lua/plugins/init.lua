f6c8df2c679633075e06c7ec7cc1ee860630abe6
luissena <luis.senarode@gmail.com>
2026-01-31 08:13:20 -0300

    feat(plugins): add plugin aggregator for modular loading

---
+++ lua/plugins/init.lua
@@ -0,0 +1,10 @@
+return {
+  require "plugins.dressing",
+  require "plugins.lazydev",
+  require "plugins.conform",
+  require "plugins.lspconfig",
+  require "plugins.roslyn",
+  require "plugins.flutter-tools",
+  require "plugins.mason",
+  require "plugins.treesitter",
+}
